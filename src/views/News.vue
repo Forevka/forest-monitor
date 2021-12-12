c8e9d19d44acf65d61f9875a70b8792fb1b204ce
Forevka <32968153+Forevka@users.noreply.github.com>
2021-12-12 11:33:06 +0000

    up

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="home-container page-container">
-
+    <div v-for="thing in news" :key="thing.id" class="thing">
+      <feed-element :id="thing.id" :description="thing.description" :author="thing.author" :title="thing.title" :image="thing.image">
+      </feed-element>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import FeedElement from '@/components/News/FeedElement.vue';
+import axios from '../utils/axios'
 
 export default defineComponent({
-  name: 'News'
+  name: 'News',
+  components: {
+    FeedElement,
+  },
+  setup() {
+    return {
+      news: ref([])
+    }
+  },
+  async beforeMount() {
+    await axios
+      .get('/news')
+      .then((response: any) => {
+        console.log(response.data)
+        this.news.push(...response.data);
+        console.log(this.news)
+      })
+  }
 })
 </script>
 
@@ -17,5 +39,9 @@ export default defineComponent({
   .vue-element-plus-logo {
     width 50%
   }
+}
+
+.thing {
+  padding: 2em;
 }
 </style>
